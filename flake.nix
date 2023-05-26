006434da825523fb0b1275090563e2e4dc37a648
Karol Baryła <karol.baryla@scylladb.com>
2023-05-26 15:12:37 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A script/library to create, launch and remove an Scylla / Apache Cassandra cluster on
+localhost.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      make_ccm_package = pkgs: pkgs.python3Packages.buildPythonApplication {
+        pname = "scylla-ccm";
+        version = "0.1";
+
+        src = ./. ;
+
+        checkInputs = with pkgs.python3Packages; [ pytestCheckHook ];
+        propagatedBuildInputs =  with pkgs.python3Packages; [ pyyaml psutil six requests packaging boto3 tqdm setuptools ];
+
+        disabledTestPaths = [ "old_tests/*.py" ];
+
+        doCheck = false;
+      };
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          scylla_ccm = make_ccm_package pkgs;
+          default = scylla_ccm;
+        };
+      }
+    ) // rec {
+      overlays.default = final: prev: {
+        scylla_ccm = make_ccm_package final;
+      };
+    };
+}
